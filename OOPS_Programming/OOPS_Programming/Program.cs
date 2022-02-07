5b1f315a52973a9a12014569f30e3680c0a5889f
Manish <manishkumar0156@gmail.com>
2022-02-07 12:35:41 +0530

    [Manish] Add: jsonInventoryManagement using DeserializeObject

---
+++ OOPS_Programming/OOPS_Programming/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPS_Programming
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+
+            InventoryManagement inventoryManagement = new InventoryManagement();
+            inventoryManagement.ReadData(@"C:\Users\kmani\Downloads\DNetFolder\OOPS_Programming\OOPS_Programming\Inventory.json");
+            Console.ReadKey();
+        }
+    }
+}
