bed37433ae00a0e53c8efc87451d0757fdc08727
Manish <manishkumar0156@gmail.com>
2022-02-09 12:44:57 +0530

    [Manish] Add: added stock account management problem

---
+++ StockAccountManagement/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StockAccountManagement
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            StockManagement stockManagement = new StockManagement();
+            stockManagement.Report(@"C:\Users\kmani\Downloads\DNetFolder\StockAccountManagement\Stock.json");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ StockAccountManagement/StockManagement.cs
@@ -0,0 +1,41 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StockAccountManagement
+{
+    public class StockManagement
+    {
+        public void Report(string filepath)
+        {
+            try
+            {
+                using (StreamReader reader = new StreamReader(filepath))
+                {
+                    var json = reader.ReadToEnd();
+                    var items = JsonConvert.DeserializeObject<List<CalculateStock>>(json);
+                    Console.WriteLine("Name\t" + "NumOfShere\t" + "SherePrice\t" + "totalAmount");
+                    int totalValueOfStock=0; 
+                    foreach (var data in items)
+
+                    {
+                        Console.WriteLine(data.Name + "\t" + data.NumOfShere + "\t" + data.SherePrice + "\t" + data.NumOfShere * data.SherePrice);
+                         
+                        totalValueOfStock += data.NumOfShere * data.SherePrice;
+                    }
+                    Console.WriteLine("total value of stock ",totalValueOfStock);
+                    
+
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
